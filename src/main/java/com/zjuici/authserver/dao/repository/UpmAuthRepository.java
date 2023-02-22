c36ed357611346331ca7633e8d2abadf4dbbce2e
fuzeqiang <funs690@hotmail.com>
2023-02-22 10:28:52 +0800

    [chg] bug fix

--- src/main/java/com/zjuici/authserver/dao/repository/UpmAuthRepository.java
+++ src/main/java/com/zjuici/authserver/dao/repository/UpmAuthRepository.java
@@ -2,6 +2,7 @@ package com.zjuici.authserver.dao.repository;
 
 import com.zjuici.authserver.dao.po.UpmAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @ClassName UpmAuthRepository
@@ -10,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Date 2023/2/7 17:34
  * @Version 1.0
  */
+@Repository
 public interface UpmAuthRepository extends JpaRepository<UpmAuth, String> {
 }
